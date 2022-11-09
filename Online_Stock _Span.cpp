d2cbb8cdf15da729a9febbfa87bb9ab4fa7804ba
Srijan <srijanrai786@outlook.com>
2022-11-10 00:35:34 +0530

    solution added

---
+++ Online_Stock _Span.cpp
@@ -0,0 +1,25 @@
+class StockSpanner {
+public:
+    stack<pair<int,int>>s;
+    StockSpanner() {
+         
+    }
+    
+    int next(int price) {
+        int c = 1 ; 
+       
+        while(s.size() > 0 && s.top().first <= price)
+        {
+            c+= s.top().second; 
+            s.pop() ; 
+        }
+        s.push({price,c});
+        return c ; 
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
